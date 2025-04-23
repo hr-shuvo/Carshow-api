d8172dc2d01dd45c451ad44f638000f08711d6f1
hr-shuvo <h.r.shuvo9@gmail.com>
2025-04-23 18:34:44 +0600

    fix config

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -3,10 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AuctionDbContext>(opt =>
@@ -26,7 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+app.MapControllers();
 
 try
 {
