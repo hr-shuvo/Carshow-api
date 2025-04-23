d14735f34770fb1b0feed0934c6a9009447bc7d5
hr-shuvo <h.r.shuvo9@gmail.com>
2025-04-23 08:13:49 +0600

    add dtos

---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,20 @@
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<AuctionCreateDto, Auction>()
+            .ForMember(dest => dest.Item, opt => opt.MapFrom(src => src));
+        
+        CreateMap<AuctionCreateDto, Item>();
+        
+    }
+}
