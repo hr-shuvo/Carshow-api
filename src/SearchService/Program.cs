b66626f90ac74cb69e0b7fbbca873acf6c6af97e
hr-shuvo <h.r.shuvo9@gmail.com>
2025-04-24 17:11:04 +0600

    add search service (another micro service project)

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+}
+
+// app.UseHttpsRedirection();
+
+app.MapControllers();
+
+
+app.Run();
+
